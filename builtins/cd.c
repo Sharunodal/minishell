52c85af0e3e999dc0eb3a97aafc23afc5bb90531
jmouette <jmouette@student.hive.fi>
2024-10-30 17:00:24 +0100

    added ~/

--- builtins/cd.c
+++ builtins/cd.c
@@ -6,7 +6,7 @@
 /*   By: jmouette <jmouette@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/03 15:25:42 by jmouette          #+#    #+#             */
-/*   Updated: 2024/10/16 14:06:46 by jmouette         ###   ########.fr       */
+/*   Updated: 2024/10/30 16:52:18 by jmouette         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -67,6 +67,37 @@ static int	check_cd(t_token **token, int i, char **new_path)
 	return (0);
 }
 
+static int	expand_home_path(t_token **token, int i, char **new_path)
+{
+	char	*home;
+	char	expanded_path[260];
+
+	if (token[i + 1] && token[i + 1]->type == 2)
+	{
+		ft_putstr_fd("cd: too many arguments\n", 2);
+		return (1);
+	}
+	if (ft_strncmp(token[i]->value, "~/", 2) == 0)
+	{
+		home = getenv("HOME");
+		if (home)
+		{
+			ft_strlcpy(expanded_path, home, sizeof(expanded_path));
+			ft_strlcat(expanded_path, token[i]->value + 1, sizeof(expanded_path));
+			*new_path = ft_strdup(expanded_path);
+		}
+		if (access(*new_path, F_OK) != 0)
+		{
+			ft_putstr_fd("cd: No such file or directory\n", 2);
+			return (1);
+		}
+	}
+	else
+		return (check_cd(token, i, new_path));
+	return (2);
+}
+//check if we can use the function set_environment_variable in export instead of export_cd
+
 int	handle_cd(t_token **token_group, t_var *var)
 {
 	char	new_cwd[260];
@@ -78,7 +109,7 @@ int	handle_cd(t_token **token_group, t_var *var)
 	if (!getcwd(old_path, sizeof(old_path)))
 		return (1);
 	i = find_command_index(token_group, "cd");
-	result = check_cd(token_group, i + 1, &new_path);
+	result = expand_home_path(token_group, i + 1, &new_path);
 	if (result == 0)
 		new_path = token_group[i + 1]->value;
 	else if (result == 1)
@@ -89,6 +120,7 @@ int	handle_cd(t_token **token_group, t_var *var)
 			return (1);
 		export_cd("OLDPWD", old_path, var);
 		export_cd("PWD", new_cwd, var);
+		//free (new_path);
 	}
 	else
 		return (1);
